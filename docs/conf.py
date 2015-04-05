b76d41d0d5b8a6224778faf1071ee25ae12afe00
Chris Beaumont <chrisnbeaumont@gmail.com>
2015-04-04 20:44:06 -0700

    Fix for older sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
-    'sphinx.ext.napoleon',
+    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
